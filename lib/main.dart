d36c6227a2115419a32a8f6a268908520ec168f9
hvmq <hovuminhquang179065@gmail.com>
2023-06-12 22:36:41 +0700

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:path_provider/path_provider.dart' as path;
+import 'package:todoai_flutter/models/task.dart';
+import 'package:todoai_flutter/pages/home/home_screen.dart';
+import 'package:todoai_flutter/providers/task_provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await path.getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+
+  Hive.registerAdapter<Task>(TaskAdapter());
+  await Hive.openBox<Task>('taskBox');
+
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider.value(value: TaskProvider())],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +27,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return  MaterialApp(
+       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFEEF1F8),
+        primarySwatch: Colors.blue,
+        fontFamily: "TodoAi-Bold",
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
         ),
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,     
+      ],
+  
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color(0xFFDEE3F2),
+    width: 1,
+  ),
+);
+
---
+++ lib/pages/home/home_screen.dart
@@ -0,0 +1,292 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:todoai_flutter/models/task.dart';
+import 'package:todoai_flutter/pages/home/components/list_item_widget.dart';
+import 'package:todoai_flutter/providers/task_provider.dart';
+
+import 'components/add_task.dart';
+import 'components/calendar.dart';
+import 'components/circle_progress/circle_progress.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late ConnectivityResult result;
+  late StreamSubscription subscription;
+
+var isConnected = false;
+
+  checkInternet() async {
+    final taskProvider = Provider.of<TaskProvider>(context, listen: false);
+    result = await Connectivity().checkConnectivity();
+    if (result != ConnectivityResult.none) {
+   
+      await syncTaskServer();
+    }
+    taskProvider.getTaskServer().whenComplete(() => taskProvider.getAllTask());
+    setState(() {});
+  }
+
+  startStreaming() {
+    subscription = Connectivity().onConnectivityChanged.listen((event) async {
+      await checkInternet();
+    });
+  }
+
+  Future<void> syncTaskServer() async {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      final taskProvider = Provider.of<TaskProvider>(context, listen: false);
+      for (int i = 0; i < taskProvider.tasks.length; i++) {
+        if (taskProvider.tasks[i].isDelete == true) {
+          taskProvider.deleteTaskServer(taskProvider.tasks[i], i);
+        } else if (taskProvider.tasks[i].isAdd == true) {
+          taskProvider.addTaskServer(
+              taskProvider.tasks[i].title,
+              taskProvider.tasks[i].date,
+              taskProvider.tasks[i].describe,
+              taskProvider.tasks[i].time,
+              taskProvider.tasks[i].color,
+              "6433cf38a042d150f0966572",
+              i);
+        } else if (taskProvider.tasks[i].isUpdate == true) {
+          taskProvider.updateTaskServer(taskProvider.tasks[i], i);
+        }
+      }
+    });
+
+  }
+
+  DateTime _selectedDateTime = DateTime.now();
+  void _handleDateTimeChanged(DateTime newDateTime) {
+    setState(() {
+      _selectedDateTime = newDateTime;
+    });
+  }
+
+  countTaskDontComplete(List<Task> list, String date) {
+    int countTask = 0;
+    for (int i = 0; i < list.length; i++) {
+      if (list[i].isComplete == false && list[i].date == date) {
+        countTask++;
+      }
+    }
+    return countTask;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Gọi requestFocus() khi widget được xây dựng
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<TaskProvider>(context, listen: false).getAllTask();
+      startStreaming();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final taskProvider = Provider.of<TaskProvider>(context, listen: false);
+    final String dateFormat =
+        DateFormat('dd/MM/yyyy').format(_selectedDateTime);
+
+    final String dateNowFormat =
+        DateFormat('dd/MM/yyyy').format(DateTime.now());
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Consumer<TaskProvider>(
+          builder: (context, taskData, child) => Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 20, left: 70),
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                         
+                          Text(
+                            'Xin chào 👋',
+                            style: TextStyle(
+                                fontFamily: 'TodoAi-Book', fontSize: 15),
+                          ),
+                          Text(
+                            'HOMIE',
+                            style: TextStyle(
+                                fontFamily: 'TodoAi-Bold', fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 65,
+                      width: 60,
+                      child: Stack(
+                        fit: StackFit.expand,
+                        children: [
+                          const Positioned(
+                            right: 0,
+                            height: 45,
+                            bottom: 8,
+                            child: CircleAvatar(
+                              radius: 30,
+                              backgroundImage:
+                                  AssetImage('assets/icons/avatar.png'),
+                            ),
+                          ),
+                          Positioned(
+                              bottom: 7,
+                              left: 0,
+                              child: Container(
+                                height: 18,
+                                width: 25,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(
+                                        color: Colors.white, width: 1),
+                                    color: Colors.green),
+                                child: Row(
+                                  children: [
+                                    const SizedBox(
+                                      width: 4,
+                                    ),
+                                    Image.asset('assets/icons/iconVector.png'),
+                                    const SizedBox(width: 2),
+                                    const Text(
+                                      '9',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'TodoAi-Bold',
+                                          fontSize: 12),
+                                    )
+                                  ],
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    )
+                  ],
+                ),
+              ),
+              CalendarMonth(onDateTimeChanged: _handleDateTimeChanged),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 25,
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset('assets/icons/loudspeaker_icon.png'),
+                    Text(
+                        'Bạn có ${countTaskDontComplete(taskData.tasks, dateNowFormat)} công việc cần làm trong hôm nay')
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              SingleChildScrollView(
+                child: SizedBox(
+                  height: 330,
+                  child: ListView.builder(
+                    itemCount: taskData.tasks.length,
+                    itemBuilder: (context, index) {
+                      Task task = taskData.tasks[index];
+                      if (task.isComplete == false &&
+                          task.date == dateFormat &&
+                          task.isDelete == false) {
+                        return ListItemWidget(
+                            index: index,
+                            task: task,
+                            onClicked: () async {
+                              await taskProvider.updateTask(
+                                  Task(
+                                      id: task.id,
+                                      date: task.date,
+                                      title: task.title,
+                                      isComplete: true,
+                                      describe: task.describe,
+                                      time: task.time,
+                                      color: task.color,
+                                      isAdd: task.isAdd,
+                                      isUpdate: task.isUpdate,
+                                      isDelete: task.isDelete),
+                                  index);
+                              taskProvider.getAllTask();
+                            });
+                      } else {
+                        return const SizedBox.shrink();
+                      }
+                    },
+                  ),
+                ),
+              ),
+              CircleProgress(tasks: taskData.tasks),
+              SizedBox(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: taskData.tasks.length,
+                  itemBuilder: (context, index) {
+                    Task task = taskData.tasks[index];
+                    if (task.isComplete == true && task.date == dateFormat) {
+                      return ListItemWidget(
+                          index: index,
+                          task: task,
+                          onClicked: () async {
+                            await taskProvider.updateTask(
+                                Task(
+                                    id: task.id,
+                                    date: task.date,
+                                    title: task.title,
+                                    isComplete: false,
+                                    describe: task.describe,
+                                    time: task.time,
+                                    color: task.color,
+                                    isAdd: task.isAdd,
+                                    isUpdate: task.isUpdate,
+                                    isDelete: task.isDelete),
+                                index);
+                            taskProvider.getAllTask();
+                          });
+                    } else {
+                      return const SizedBox.shrink();
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              backgroundColor: Colors.transparent,
+              context: context,
+              isScrollControlled: true,
+              builder: (context) {
+                return const AddTask();
+              });
+        },
+        child: Image.asset('assets/icons/Add_icon.png'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
